72480366afbc4cf17fa79ad8ef4bb1932781108e
jessasesh <jessica.dison@yahoo.com>
2023-12-22 20:33:24 -0800

    Missed commits

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,19 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_height, r_height;
+	size_t left_size = 0;
+	size_t right_size = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_size += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_size += 1 + binary_tree_height(tree->right);
 
-	return (l_height > r_height ? l_height + 1 : r_height + 1);
+	if (left_size > right_size)
+		return (left_size);
+	else
+		return (right_size);
 }
